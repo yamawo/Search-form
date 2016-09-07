6de66994cd70468494976096fc9773a17a6942e6
Seo Kyohei <Kyohei@Seo.local>
2016-09-07 17:07:16 +0900

    Add anser for exercise1

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 var data = ['apple', 'apricot', 'avocado', 'blueberry', 'cherry', 'coconut', 'cranberry', 'dragonfruit', 'durian', 'grape', 'grapefruit', 'guava', 'kiwi fruit', 'lemon', 'lime', 'lychee', 'mango', 'melon', 'watermelon', 'miracle fruit', 'orange', 'bloodorange','clementine','mandarine','tangerine','papaya','passionfruit','peach','pear','persimmon','physalis','plum/prune','pineapple','pomegranate','raspberry','rambutan','star fruit','strawberry'];
+
+$(function() {
+  $("#submit").on("click", function() {
+    var value = $("#keyword").val();
+    $("#result").text(value);
+  });
+});
